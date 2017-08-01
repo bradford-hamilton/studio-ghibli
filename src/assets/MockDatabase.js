61d8ae7e32327408a5f27b32cf46724e050267c0
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2017-08-01 17:13:52 -0600

    add mocks for the data I get back in first view because I’m not adding a backend/server.

---
+++ src/assets/MockDatabase.js
@@ -0,0 +1,24 @@
+const MockDatabase = {
+  0: 'Castle in the Sky',
+  1: 'Grave of the Fireflies',
+  2: 'My Neighbor Totoro',
+  3: 'Kiki\'s Delivery Service',
+  4: 'Only Yesterday',
+  5: 'Porco Rosso',
+  6: 'Pom Poko',
+  7: 'Whisper of the Heart',
+  8: 'Princess Mononoke',
+  9: 'My Neighbors the Yamadas',
+  10: 'Spirited Away',
+  11: 'The Cat Returns',
+  12: 'Howl\'s Moving Castle',
+  13: 'Tales from Earthsea',
+  14: 'Ponyo',
+  15: 'Arrietty',
+  16: 'From Up on Poppy Hill',
+  17: 'The Wind Rises',
+  18: 'The Tale of the Princess Kaguya',
+  19: 'When Marnie Was There',
+};
+
+export default MockDatabase;
---
+++ src/assets/MockFilmPosterObject.js
@@ -0,0 +1,24 @@
+const MockFilmPosterObject = {
+  0: 'http://image.tmdb.org/t/p/w342/A4w9oSNQUwtkaucy3K7LbSngKrk.jpg',
+  1: 'http://image.tmdb.org/t/p/w342/4RTG2AaqZ9eleL51ryWwv78WwDu.jpg',
+  2: 'http://image.tmdb.org/t/p/w342/mVlosCi4Kckj1iXACJdpSrA9alR.jpg',
+  3: 'https://www.purelypoultry.com/images/georgia-giant-bobwhite-quail.jpg',
+  4: 'http://image.tmdb.org/t/p/w342/iMarB2ior30OAXjPa7QIdeyUfM1.jpg',
+  5: 'http://image.tmdb.org/t/p/w342/p1mQngirSxR0B2oXWhSrf3aZVOS.jpg',
+  6: 'http://image.tmdb.org/t/p/w342/2i0OOjvi7CqNQA6ZtYJtL65P9oZ.jpg',
+  7: 'http://image.tmdb.org/t/p/w342/tuKO2PLGDcJj5vpfAYKSQcnLEpD.jpg',
+  8: 'http://image.tmdb.org/t/p/w342/ldgDYGmapFGksNQWEcOUMpoYYEO.jpg',
+  9: 'http://image.tmdb.org/t/p/w342/xbpD0l4Y4DKVMc3LjsiSCE2sC5z.jpg',
+  10: 'http://image.tmdb.org/t/p/w342/3s0mpSpLkI7cIhqWGrGbsiyvlsH.jpg',
+  11: 'http://image.tmdb.org/t/p/w342/d8J98dLZkfTpJY64VvtCOH2UkXP.jpg',
+  12: 'http://image.tmdb.org/t/p/w342/gzlJkVfWV5VEG5xK25cvFGJgkDz.jpg',
+  13: 'http://image.tmdb.org/t/p/w342/ynXoOxmDHNQ4UAy0oU6avW71HVW.jpg',
+  14: 'http://image.tmdb.org/t/p/w342/deB7rfqZXpVxdRinyaCeyhwNPbg.jpg',
+  15: 'http://image.tmdb.org/t/p/w342/liASKLjYzk6aJ4GbQDlfYfQk02h.jpg',
+  16: 'http://image.tmdb.org/t/p/w342/11Az4sMt1C9sm8atgB199Z0BsIQ.jpg',
+  17: 'http://image.tmdb.org/t/p/w342/3OTcCyf7QbGdVxsS8GLtaSsTZtP.jpg',
+  18: 'http://image.tmdb.org/t/p/w342/wiEzIu4bw15RhB3NT58mCbPgv3R.jpg',
+  19: 'http://image.tmdb.org/t/p/w342/uPLuwtMQm3wDTKm7cZQHsszYGRo.jpg',
+};
+
+export default MockFilmPosterObject;
